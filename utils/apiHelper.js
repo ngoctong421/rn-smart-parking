779a3e829d6de3972657edab69f62d45e7a162ea
kdegit <callmekai9124@gmail.com>
2020-05-21 09:20:40 +0700

    Update

--- utils/apiHelper.js
+++ utils/apiHelper.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { AsyncStorage } from 'react-native';
 
 const instance = axios.create({
-  baseURL: 'http://1ae83229.ngrok.io',
+  baseURL: 'http://b504e303.ngrok.io',
 });
 
 instance.interceptors.request.use(
